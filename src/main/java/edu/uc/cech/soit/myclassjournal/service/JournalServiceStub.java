f7fa84e4f4d80224c03c3f69f53edea957d93a83
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2022-06-15 19:46:19 -0400

    All tests are passing now

--- src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalServiceStub.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalServiceStub.java
@@ -2,28 +2,25 @@ package edu.uc.cech.soit.myclassjournal.service;
 
 import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class JournalServiceStub implements IJournalService {
-
+    List<JournalEntry> journalEntryList = new ArrayList<>();
 
     @Override
     public void save(JournalEntry journalEntry) {
-
-    }
-
-    @Override
-    public List<JournalEntry> fetchAll() {
-        List<JournalEntry> journalEntryList = new ArrayList<>();
-
         JournalEntry journalentry = new JournalEntry();
         journalentry.setDate("October 2021");
         journalentry.setNotes("My first entry!");
         journalEntryList.add(journalentry);
+    }
 
+    @Override
+    public List<JournalEntry> fetchAll() {
         return journalEntryList;
     }
 }
